954e11dc366058a161ae9ed802a0e5940b711a8b
sahidularif <sahidularif1@gmail.com>
2021-02-08 23:43:22 +0600

    Final update and finish the project

--- js/app.js
+++ js/app.js
@@ -1,61 +1,45 @@
 
+// Themealdb API & Input Value
+const NameApi = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+const letterApi = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
+const idApi = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
+
 // Search Meal Result..
 function searchMealItem() {
     const searchKeyword = document.getElementById("search-keyword").value;
-
+    
     if (searchKeyword !== '') {
         document.getElementById('error-msg').innerText = '';
-
-        fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=' + searchKeyword + '')
-            .then(res => res.json())
-            .then(data => getMealList(data))
-
-        let parentDiv = document.getElementById("parent-div").innerHTML = ""; //msg
-        let displayMealDetails = document.getElementById("display-meal-details").innerHTML = "";
-
-        const getMealList = data => {
-
-            if (data.meals == null) {
-                document.getElementById('error-msg').innerText = 'No meals found...!!!';
-            } else {
-                data.meals.forEach(meal => {
-
-                    let parentDiv = document.getElementById("parent-div");
-                    const mealItem = document.createElement('div');
-                    let mealInfo = `
-                    
-                                <div onclick="displayMealDetails('${meal.idMeal}')" id="meal-details" >                               
-                                    <div class="meal-img" id="meal-img"> 
-                                        <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-                                    </div>
-                                    <div class="meal-name">
-                                        <h4>${meal.strMeal}</h4>                                        
-                                    </div>
-                                </div> `;
-
-                    mealItem.innerHTML = mealInfo;
-                    parentDiv.appendChild(mealItem);
-                });
-            }
+        
+        if(searchKeyword.length == 1){
+           let url = `${letterApi}${searchKeyword}`;
+           searchMeal(url);
         }
-    } else {
-        console.log('Please enter your search name');
+        else{
+            let url = `${NameApi}${searchKeyword}`;
+            searchMeal(url);
+        }
+
+    } 
+    else {
+        // Error message if input text is empty        
         let msg = document.getElementById('error-msg');
         msg.innerText = 'Please type your search keyword !!!'
-        let parentDiv = document.getElementById("parent-div").innerHTML = ""; //msg
+        let parentDiv = document.getElementById("parent-div").innerHTML = ""; 
     }
 
 }
 
-// Display Meal Details
+// Display meal details API
+
 const displayMealDetails = idMeal => {
     let displayMealDetails = document.getElementById("display-meal-details").innerHTML = "";
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    fetch(`${idApi}${idMeal}`)
         .then(response => response.json())
         .then(data => showMealRecipe(data.meals));
 }
 
-// Display Meals Details
+// Display Meals Details Function
 function showMealRecipe(meal) {
     meal = meal[0];
     let displayMealDetails = document.getElementById('display-meal-details');
@@ -68,7 +52,7 @@ function showMealRecipe(meal) {
             );
         } 
         else {
-            // Stop if there are no more ingredients
+           
             break;
         }
     }
@@ -79,11 +63,51 @@ function showMealRecipe(meal) {
                     </div>
                     <div id="meal-name">
                         <h3>${meal.strMeal}</h3>
-                        <h5>Ingredients</h5>                        
-                        ${ingredients.map(ingredient => `<li id="ingredient"><i class="far fa-check-circle"></i> &nbsp; ${ingredient}</li>`).join('')}                                                   
+                        <h5>Ingredients</h5>
+                        
+                        ${ingredients.map(ingredient => `<li id="ingredient"><i class="far fa-check-circle"></i> &nbsp; ${ingredient}</li>`).join('')}
+                                               
+                        
                     </div>
                 </div>
             `;
-    
     displayMealDetails.appendChild(displayMeal);
 }
+
+// Search User Meals
+
+function searchMeal(url) {
+    fetch(url)
+        .then(res => res.json())
+        .then(data => getMealList(data))
+
+    let parentDiv = document.getElementById("parent-div").innerHTML = ""; 
+    let displayMealDetails = document.getElementById("display-meal-details").innerHTML = "";
+
+    const getMealList = data => {
+
+        if (data.meals == null) {
+            document.getElementById('error-msg').innerText = 'No meals found...!!!';
+        } 
+        else {
+            data.meals.forEach(meal => {
+
+                let parentDiv = document.getElementById("parent-div");
+                const mealItem = document.createElement('div');
+                let mealInfo = `            
+                        <div onclick="displayMealDetails('${meal.idMeal}')" id="meal-details" >
+                       
+                            <div class="meal-img" id="meal-img"> 
+                                <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                            </div>
+                            <div class="meal-name">
+                                <h4>${meal.strMeal}</h4>                                        
+                            </div>
+                        </div> `;
+                mealItem.innerHTML = mealInfo;
+                parentDiv.appendChild(mealItem);
+            });
+        }
+    }
+}
+
